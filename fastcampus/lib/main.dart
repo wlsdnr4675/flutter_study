b346b6234c6eeb5cd247887d65b87c1adaa3f58f
Jinwook <wlsdnr4675@gmail.com>
2022-06-01 17:52:29 +0900

    feat: test

--- fastcampus/lib/main.dart
+++ fastcampus/lib/main.dart
@@ -49,6 +49,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _idx = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -71,45 +72,46 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("패스트 캠퍼스 테스트"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 1,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              child: Text("이름"),
+              margin: EdgeInsets.all(16),
+              padding: EdgeInsets.all(16),
+              color: Colors.blue,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text("이름"),
+            ListTile(
+              title: Text("이름"),
+              trailing: Text("오진욱"),
+              onTap: () => {print("오진욱")},
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => {print("FAB 눌림")},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "HOME"),
+          BottomNavigationBarItem(icon: Icon(Icons.more), label: "더보기 탭"),
+          BottomNavigationBarItem(icon: Icon(Icons.more), label: "더보기 탭2"),
+        ],
+        onTap: (index) => {
+          setState(
+            () => {_idx = index},
+          )
+        },
+        currentIndex: _idx,
+      ),
     );
   }
 }
